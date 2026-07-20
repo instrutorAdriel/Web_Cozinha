b451db03a165f2f938bb03036e000a27c9e0d461
JankledsonRodriguez <jankledson.rodrigues@gmail.com>
2026-07-20 09:35:54 -0300

    Atualização para Web

--- src/main/java/com/application/WebApplicationSIGEC/controller/ReceitaController.java
+++ src/main/java/com/application/WebApplicationSIGEC/controller/ReceitaController.java
@@ -1,4 +1,4 @@
-package com.example.primeiroAppSpring.controller;
+package com.application.WebApplicationSIGEC.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
